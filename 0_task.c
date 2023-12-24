04217f8161a47a195991274ef16b112c81f24d32
Salma Saeed Mohamed <salmasa3ed12@gmail.com>
2023-12-24 06:47:16 -0800

    TASK_0

---
+++ 0_task.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char str[] = "HELLO TASH ZERO\n";
+int i = 0;
+while (str[i])
+{
+putchar(str[i]);
+i++;
+}
+return (0);
+}
