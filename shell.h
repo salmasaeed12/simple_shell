acabbfc7f9b3b3b5e7e5090957c30b46e9ec1e97
saramohamed124 <saramohamaden1234@gmail.com>
2024-01-04 12:36:55 -0800

    shell.h1

---
+++ shell.h
@@ -0,0 +1,97 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+
+/* For read or write buffers */
+#define READ_BUF_SIZE 1024
+#define WRITE_BUF_SIZE 1024
+#define BUF_FLUSH -1
+
+/* For command chaining */
+#define CMD_NORM 0
+#define CMD_OR 1
+#define CMD_AND 2
+#define CMD_CHAIN 3
+
+/* For convert_number() */
+#define CONVERT_LOWERCASE 1
+#define CONVERT_UNSIGNED 2
+
+/* 1 if using system getline() */
+#define USE_GETLINE 0
+#define USE_STRTOK 0
+
+#define HIST_FILE ".simple_shell_history"
+#define HIST_MAX
+
+extern char **environ;
+
+/**
+ * struct liststr - singly linked list
+ * @num: the number of field
+ * @str: a string
+ * @next: points to the next node
+ */
+typedef struct liststr
+{
+    int num;
+    char *str;
+    struct liststr *next;
+} list_t;
+
+/**
+ *struct passinfo - contains pseudo-arguments to pass into a func
+ *@arg: a string genreated from getline
+ *@argv: an array of strings generated from arg
+ *@path: a string path for the current command
+ *@argc: the argument count
+ *@line_count: the error count
+ *@err_num: the error code for exit() s
+ *@linecount_flag: if on count thes line of input
+ *@fname: the program filename
+ *@env: linked list local comy of environm
+ *@environ: custom modified copy of environ from LL env
+ *@history: the history node
+ *@alias: the alias node
+ *@env_changed: on if environ was changed
+ *@status: the return status of the last exec'd command
+ *@cmd_buf: address of pointer to cmd_buf, on if chaining
+ *@cmd_buf_type: CMD_type ||, &&, ;
+ *@readfd: the fd from which to read line input
+ *@histcount: the history line number count
+ */
+
+typedef struct passinfo
+{
+    char *arg;
+    char **argv;
+    char *path;
+    int argc;
+    unsigned int line_count;
+    int err_num;
+    int linecount_flag;
+    char *fname;
+    list_t *env;
+    list_t *history;
+    list_t *alias;
+    char **environ;
+    int env_changed;
+    int status;
+
+    char **cmd_buf; /* pointer to cmd ; chain buf, for memory management*/
+    int cmd_buf_type;
+    int readfd;
+    int histcount;
+} info_t;
+
+#endif
